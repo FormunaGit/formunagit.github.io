6bb3691c3c262f39cc210707b80133796b199f88
FormunaGit <formuna@tuta.io>
2024-11-24 13:25:58 -0400

    init the sequel

---
+++ main.js
@@ -0,0 +1,4 @@
+console.log('What are you doing here?');
+console.log('Searching for bugs? I don\'t have money, so don\'t try being a bug-hunter...');
+console.log('Wanna do it for free? Make an issue on this GitHub repo:');
+console.warn('https://github.com/FormunaGit/formunagit.github.io/issues/')
